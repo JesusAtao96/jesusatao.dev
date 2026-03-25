406adbc5242b9d01c78dcfc810f61a1771589d57
Jesús Alejandro Atao <jealeat96@gmail.com>
2026-03-25 02:48:49 -0500

    feat: Add new `og-image.jpg`, update `astro.config.mjs` site URL, and adjust `Layout.astro` to use the new image and `Astro.site` for OG image pathing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jesus-atao.vercel.app',
+  site: 'https://jesusatao.dev',
   vite: {
     plugins: [tailwindcss()]
   },
